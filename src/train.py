8accbaf7024cab186f981560ead5f39a8c64e66e
agusabdulrahman <agusabdulrahman069@gmail.com>
2024-07-28 21:07:02 +0700

    add models

--- src/train.py
+++ src/train.py
@@ -7,3 +7,24 @@ from typing import Tuple
 import numpy as np
 import ray
 import ray.train as train
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import typer 
+from ray.air.integrations.mlflow import MLflowLoggerCallback
+from ray.data import Dataset
+from ray.train import (
+    Checkpoint,
+    CheckpointConfig,
+    DataConfig,
+    RunConfig,
+    ScalingConfig
+)
+
+
+
+app = typer.Typer(
+    
+)
+
+def train_step() - float:
